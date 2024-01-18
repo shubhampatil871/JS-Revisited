cd203764efcbeac476447b4af7bd4bb384139879
Shubham Patil <52715570+shubhampatil871@users.noreply.github.com>
2024-01-18 15:40:48 +0000

    working with data types today

---
+++ 01_Basics/dataTypes.js
@@ -0,0 +1,13 @@
+//there are multiple data types in js 
+
+const number = 456
+let stringVariable = "shubham"
+let isLoggedIn = true
+let ThisIsNull = null
+let unDefinedVar
+
+console.log(number)
+console.log(stringVariable)
+console.log(isLoggedIn)
+console.log(ThisIsNull)
+console.log(unDefinedVar)
