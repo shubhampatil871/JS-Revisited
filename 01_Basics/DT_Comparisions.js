18f19c5f44cbe069ab795918fe249def23ab56af
Shubham Patil <52715570+shubhampatil871@users.noreply.github.com>
2024-01-24 11:33:13 +0000

    learning about data conversions and coercions

---
+++ 01_Basics/DT_Comparisions.js
@@ -0,0 +1,10 @@
+// console.log(4 > 3);
+// console.log(4 < 3);
+// //console.log(4 = 3); // you cannot assign values like this
+// console.log(4 == 4);
+// console.log(4 === 4); //strictly checks the value as well as data type of the variables being compared
+// console.log(5 != 5);
+
+console.log("3" > 1);
+console.log(1 > "3");
+console.log("2" === 2);
---
+++ 01_Basics/dataTypeConversion.js
@@ -0,0 +1,22 @@
+let myName = "shubham";
+let firstNumber = 347;
+let isLoggedIn = false;
+let nullVariable = null;
+
+// console.log(myName);
+// console.log(firstNumber);
+// console.log(typeof myName);
+// console.log(typeof firstNumber);
+// console.log(isLoggedIn);
+// console.log(typeof isLoggedIn);
+// console.log(nullVariable);
+// console.log(typeof nullVariable);
+
+// console.log(2 + 2);
+// console.log("2" + 2);
+// console.log(2 + "2");
+// console.log("2" + "2");
+// console.log(2 + 2 + 3);
+// console.log("2" + 2 + 3);
+// console.log(2 + "2" + 3);
+// console.log(2 + 2 + "3");
